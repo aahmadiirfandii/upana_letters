9f347555ae709787cb7493cab49db3b556117164
Ahmad Irfandi <ahmad7irfandi@gmail.com>
2019-06-24 10:22:59 +0800

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/persuratan';
+$config['base_url'] = 'http://localhost:8080/persuratan_upana';
 
 /*
 |--------------------------------------------------------------------------
